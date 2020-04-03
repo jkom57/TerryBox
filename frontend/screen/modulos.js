7102190e1dfbe2d9472a5d1c305d76b563e0881c
Joshua Komaba Chávez <joshuakomaba@gmail.com>
2020-04-02 21:05:46 -0700

    novena version

--- frontend/screen/modulos.js
+++ frontend/screen/modulos.js
@@ -1,28 +1,84 @@
 import React, {Component} from 'react';
-import {Button, View, Text, StyleSheet, SafeAreaView} from 'react-native';
+import {Button, View, Text, StyleSheet, ScrollView} from 'react-native';
+import HTMLView from 'react-native-htmlview';
+import RENDERHTML from 'react-native-render-html';
+import { WebView } from 'react-native-webview';
 
-export default class About extends Component{
+var HTML = '<iframe data="https://asksensors.com/actuatorfs.html?s=4251&m=7676&uid=W0OashokGXvcq97CJrUgdFOvCBSQC0" width="400" height="300" type="text/html" />'
+var HTML2 = '<iframe data="https://asksensors.com/graphfs.html?s=4250&m=7674&uid=8POPafmnh9EECjGzDO8AEUEWGSIUt7" width="400" height="300" type="text/html">algo</iframe>'
+const HTML3 = '<object data="https://asksensors.com/graphfs.html?s=4250&m=7675&uid=DWvGeIAfKyJT33UIRUH3pmhiWz06y1" width="400" height="300" type="text/html">algo</object>'
+const HTML4 = '<a href="https://asksensors.com/actuatorfs.html?s=4251&m=7676&uid=W0OashokGXvcq97CJrUgdFOvCBSQC0">Aiuda</a>'
+
+//var lista = document.getElementById("lista");
+//lista.insertAdjacentHTML("beforeend", "Elemento");
+
+/*
+ import { WebView } from 'react-native-webview';
+
+class MyInlineWeb extends Component {
+  render() {
+    return (
+      <WebView
+        originWhitelist={['*']}
+        source={{ html: "<iFrame src='your_URL' ></WebView>" }}
+      />
+    );
+  }
+}
+ 
+
+import React, { Component } from 'react';
+import { WebView } from 'react-native-webview';
+
+class MyInlineWeb extends Component {
+  render() {
+    return (
+      <WebView
+        originWhitelist={['*']}
+        source={{ html: '<h1>Hello world</h1>' }}
+      />
+    );
+  }
+}
+ */
+
+export default class About extends Component{ 
   render(){
     return(
-        <SafeAreaView style={styles.container}>
-            <View style={styles.modulos}>
-              <View style={styles.modulos}>
-                  <Text style={styles.mod}>Módulo 1</Text>
-                  <Text style={styles.info}>Usuario: </Text>
-                  <Text style={styles.info}>Token: </Text>
-                  <Button color={'rgb(0,230,0)'} title="Status: Ok" style={styles.stat}/>
-              </View>
-              <View style={styles.modulos}>
-                  <Text style={styles.mod}>Módulo 2</Text>
-                  <Text style={styles.info}>Usuario: </Text>
-                  <Text style={styles.info}>Token: </Text>
-                  <Button color={'rgb(0,230,0)'} title="Status: Ok" style={styles.stat}/>
-              </View>
-            </View>
-        </SafeAreaView>
+        //<ScrollView style={styles.container}>
+        /*<View style={styles.modulos}>
+        <View style={styles.modulos}>
+            <Text style={styles.mod}>Módulo 1</Text>
+            <Text style={styles.info}>Usuario: </Text>
+            <Text style={styles.info}>Token: </Text>
+            <Button color={'rgb(0,230,0)'} title="Status: Ok" style={styles.stat}/>
+        </View>
+        <View style={styles.modulos}>
+            <Text style={styles.mod}>Módulo 2</Text>
+            <Text style={styles.info}>Usuario: </Text>
+            <Text style={styles.info}>Token: </Text>
+            <Button color={'rgb(0,230,0)'} title="Status: Ok" style={styles.stat}/>
+        </View>
+        <HTMLView value={HTML}></HTMLView>
+        <HTMLView value={HTML2}></HTMLView>
+        <HTMLView value={HTML3}></HTMLView>
+        <RENDERHTML html={HTML}></RENDERHTML>
+        <RENDERHTML html={HTML4}></RENDERHTML>
+        <WebView
+          originWhitelist={['*']}
+              source={{ html: "<h1>checando si funciona web view<h1>"}}
+        />
+      </View>
+      */
+            <WebView
+                originWhitelist={['*']}
+                source={{ html: '<iframe src="https://asksensors.com/actuatorfs.html?s=4251&m=7676&uid=W0OashokGXvcq97CJrUgdFOvCBSQC0" width="400" height="300" type="text/html" />' }}
+            />
+        //</ScrollView>
     )
   }
 }
+//bueno, hare un cambio temporal, no te asustes
 
 const styles = StyleSheet.create({
     container: {
