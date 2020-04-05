70a750a97fae65ce3ec09be76f8e01d20caac007
Joshua Komaba Chávez <joshuakomaba@gmail.com>
2020-04-05 13:59:02 -0600

    App Móvil

--- frontend/App.js
+++ frontend/App.js
@@ -1,88 +1,87 @@
-import * as React from 'react';
-import {Button} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Registrarse from './screen/register';
-import Sesion from './screen/login';
+import * as React from 'react'
+import {Button} from 'react-native'
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
 import Inicio from './screen/index';
-import Modulos from './screen/modulos';
 import Perfil from './screen/perfil';
+import Modulos from './screen/modulos';
+import Sesion from './screen/login';
+import Registrar from './screen/register';
 
-const MainStack = createStackNavigator();
-const RootStack = createStackNavigator();
+const MainStack = createStackNavigator()
+const RootStack = createStackNavigator()
 
 function MainStackScreen() {
-  return (
-    <MainStack.Navigator>
-      <MainStack.Screen name="Inicio" component={Inicio} 
-        options={{
-          title: 'TerryBox',
-          headerStyle: {
-            backgroundColor: 'rgb(7,89,212)',
-          },
-          headerTitleStyle: {
-            fontSize: 30,
-          },
-          headerRight: () => (
-            <Button
-              onPress={() =>  alert('This is a button!')}
-              title="Cerrar Sesión"
-              color="rgb(200,0,0)"
+    return (
+        <MainStack.Navigator>
+            <MainStack.Screen name="Inicio" component={Inicio}
+            options={{
+                title: 'TerryBOX',
+                headerStyle: {
+                    backgroundColor: 'rgb(7,89,212)',
+                },
+                headerTitleStyle: {
+                    fontSize: 30,
+                },
+                headerRight: () => (
+                    <Button
+                    onPress={() => alert('Soy un boton')}
+                    title="Cerrar Sesión"
+                    color="rgb(200,0,0)"
+                    />
+                ),
+            }}
             />
-          ),
-        }}
-      />
-      <MainStack.Screen name="Perfil" component={Perfil} 
-        options={{
-          title: 'TerryBox',
-          headerStyle: {
-            backgroundColor: 'rgb(7,89,212)',
-          },
-          headerTitleStyle: {
-            fontSize: 30,
-          },
-          headerRight: () => (
-            <Button
-              onPress={() => alert('This is a button!')}
-              title="Cerrar Sesión"
-              color="rgb(200,0,0)"
+            <MainStack.Screen name="Perfil" component={Perfil}
+            options={{
+                title: 'TerryBOX',
+                headerStyle: {
+                    backgroundColor: 'rgb(7,89,212)',
+                },
+                headerTitleStyle: {
+                    fontSize: 30,
+                },
+                headerRight: () => (
+                    <Button
+                    onPress={() => alert('Soy un boton')}
+                    title="Cerrar Sesión"
+                    color="rgb(200,0,0)"
+                    />
+                ),
+            }}
             />
-          ),
-        }}
-      />
-      <MainStack.Screen name="Modulos" component={Modulos} 
-        options={{
-          title: 'TerryBox',
-          headerStyle: {
-            backgroundColor: 'rgb(7,89,212)',
-          },
-          headerTitleStyle: {
-            fontSize: 30,
-          },
-          headerRight: () => (
-            <Button
-              onPress={() => alert('This is a button!')}
-              title="Cerrar Sesión"
-              color="rgb(200,0,0)"
+            <MainStack.Screen name="Modulos" component={Modulos}
+            options={{
+                title: 'TerryBOX',
+                headerStyle: {
+                    backgroundColor: 'rgb(7,89,212)',
+                },
+                headerTitleStyle: {
+                    fontSize: 30,
+                },
+                headerRight: () => (
+                    <Button
+                    onPress={() => alert('Soy un boton')}
+                    title="Cerrar Sesión"
+                    color="rgb(200,0,0)"
+                    />
+                ),
+            }}
             />
-          ),
-        }}
-      />
-    </MainStack.Navigator>
-  );
+        </MainStack.Navigator>
+    )
 }
 
 function App() {
-  return (
-    <NavigationContainer>
-      <RootStack.Navigator mode="modal" headerMode="none" initialRouteName='InicioDeSesion'>
-        <RootStack.Screen name="Registro" component={Registrarse} />
-        <RootStack.Screen name="InicioDeSesion" component={Sesion} />
-        <RootStack.Screen name="Main" component={MainStackScreen} />
-      </RootStack.Navigator>
-    </NavigationContainer>
-  );
+    return(
+        <NavigationContainer>
+            <RootStack.Navigator mode="modal" headerMode="none" initialRouteName='InicioDeSesion'>
+                <RootStack.Screen name="InicioDeSesion" component={Sesion} />
+                <RootStack.Screen name="Registro" component={Registrar} />
+                <RootStack.Screen name="Main" component={MainStackScreen} />
+            </RootStack.Navigator>
+        </NavigationContainer>
+    )
 }
 
-export default App;
-
+export default App
--- frontend/screen/modulos.js
+++ frontend/screen/modulos.js
@@ -1,116 +1,20 @@
 import React, {Component} from 'react';
-import {Button, View, Text, StyleSheet, ScrollView} from 'react-native';
-import HTMLView from 'react-native-htmlview';
-import RENDERHTML from 'react-native-render-html';
+import {Button, View, Text, StyleSheet, ScrollView, SafeAreaView} from 'react-native';
 import { WebView } from 'react-native-webview';
-
-var HTML = '<iframe data="https://asksensors.com/actuatorfs.html?s=4251&m=7676&uid=W0OashokGXvcq97CJrUgdFOvCBSQC0" width="400" height="300" type="text/html" />'
-var HTML2 = '<iframe data="https://asksensors.com/graphfs.html?s=4250&m=7674&uid=8POPafmnh9EECjGzDO8AEUEWGSIUt7" width="400" height="300" type="text/html">algo</iframe>'
-const HTML3 = '<object data="https://asksensors.com/graphfs.html?s=4250&m=7675&uid=DWvGeIAfKyJT33UIRUH3pmhiWz06y1" width="400" height="300" type="text/html">algo</object>'
-const HTML4 = '<a href="https://asksensors.com/actuatorfs.html?s=4251&m=7676&uid=W0OashokGXvcq97CJrUgdFOvCBSQC0">Aiuda</a>'
-
-//var lista = document.getElementById("lista");
-//lista.insertAdjacentHTML("beforeend", "Elemento");
-
-/*
- import { WebView } from 'react-native-webview';
-
-class MyInlineWeb extends Component {
-  render() {
-    return (
-      <WebView
-        originWhitelist={['*']}
-        source={{ html: "<iFrame src='your_URL' ></WebView>" }}
-      />
-    );
-  }
-}
- 
-
-import React, { Component } from 'react';
-import { WebView } from 'react-native-webview';
-
-class MyInlineWeb extends Component {
-  render() {
-    return (
-      <WebView
-        originWhitelist={['*']}
-        source={{ html: '<h1>Hello world</h1>' }}
-      />
-    );
-  }
-}
- */
 
 export default class About extends Component{ 
   render(){
     return(
-        //<ScrollView style={styles.container}>
-        /*<View style={styles.modulos}>
-        <View style={styles.modulos}>
-            <Text style={styles.mod}>Módulo 1</Text>
-            <Text style={styles.info}>Usuario: </Text>
-            <Text style={styles.info}>Token: </Text>
-            <Button color={'rgb(0,230,0)'} title="Status: Ok" style={styles.stat}/>
-        </View>
-        <View style={styles.modulos}>
-            <Text style={styles.mod}>Módulo 2</Text>
-            <Text style={styles.info}>Usuario: </Text>
-            <Text style={styles.info}>Token: </Text>
-            <Button color={'rgb(0,230,0)'} title="Status: Ok" style={styles.stat}/>
-        </View>
-        <HTMLView value={HTML}></HTMLView>
-        <HTMLView value={HTML2}></HTMLView>
-        <HTMLView value={HTML3}></HTMLView>
-        <RENDERHTML html={HTML}></RENDERHTML>
-        <RENDERHTML html={HTML4}></RENDERHTML>
-        <WebView
-          originWhitelist={['*']}
-              source={{ html: "<h1>checando si funciona web view<h1>"}}
-        />
-      </View>
-      */
-            <WebView
-                originWhitelist={['*']}
-                source={{ html: '<iframe src="https://asksensors.com/actuatorfs.html?s=4251&m=7676&uid=W0OashokGXvcq97CJrUgdFOvCBSQC0" width="400" height="300" type="text/html" />' }}
-            />
-        //</ScrollView>
+      <WebView
+        originWhitelist={['*']}
+        source={
+          { html:
+            `<iframe src="https://asksensors.com/actuatorfs.html?s=4251&m=7676&uid=W0OashokGXvcq97CJrUgdFOvCBSQC0" width="950" height="500" type="text/html" ></iframe>
+            <iframe src="https://asksensors.com/graphfs.html?s=4250&m=7674&uid=8POPafmnh9EECjGzDO8AEUEWGSIUt7" width="950" height="500" type="text/html" ></iframe>
+            <iframe src="https://asksensors.com/graphfs.html?s=4250&m=7675&uid=DWvGeIAfKyJT33UIRUH3pmhiWz06y1" width="950" height="500" type="text/html" ></iframe>`
+          }
+        }
+      />
     )
   }
 }
-//bueno, hare un cambio temporal, no te asustes
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: 'white',
-    },
-    h: {
-      flex: 0,
-      backgroundColor: 'rgb(7,89,212)',
-      alignItems: 'flex-start',
-      paddingTop: 15,
-      paddingLeft: 5,
-    },
-    text:{
-      color: 'black',
-      margin: 10,
-      fontSize: 30,
-      textAlign: 'center',
-    },
-    modulos:{
-        borderWidth: 1,
-        margin: 25,
-    },
-    mod:{
-        fontWeight: 'bold',
-        fontSize: 18,
-        marginLeft: 5,
-    },
-    info:{
-        marginLeft: 5,
-    },
-    stat:{
-        textDecorationColor: 'black',
-    }
-});
